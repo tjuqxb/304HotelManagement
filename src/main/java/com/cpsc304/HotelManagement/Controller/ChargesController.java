8ef51b1c3ab10444578f7a08337fa8c73a8fc986
YoonhaK <kyh4541@gmail.com>
2020-11-16 19:50:26 -0800

    hotelstaff/chargest controller

--- src/main/java/com/cpsc304/HotelManagement/Controller/ChargesController.java
+++ src/main/java/com/cpsc304/HotelManagement/Controller/ChargesController.java
@@ -20,7 +20,21 @@ public class ChargesController {
     final ObjectMapper mapper = new ObjectMapper(); // jackson's objectmapper
 
     @GetMapping(value = "/list")
-    public List<Map<String,Object>> getGuests() {
+    public List<Map<String,Object>> getCharges() {
         return ChargesHandler.getAllCharges();
     }
+
+
+    /*@PostMapping(value = "/add_charges")
+    public void addCharges(@RequestBody Map<String,Object> jsonStr) {
+        //System.out.println(jsonStr);
+        Integer bill_num = (Integer)(jsonStr.get("bill_num"));
+        Integer cid = (Integer)(jsonStr.get("cid"));
+        String service_name = (String)(jsonStr.get("service_name"));
+
+        if(bill_num != null && cid!= null && service_name != null) {
+            Charges c = new Charges(bill_num,cid,service_name);
+            ChargesHandler.InsertCharge(c);
+        }
+    }*/
 }
